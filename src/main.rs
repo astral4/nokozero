15d6be4fe5ac20653e365dc2152e172de9194b49
astral4 <88992929+astral4@users.noreply.github.com>
2025-06-22 17:24:54 -0400

    parse game directory from input args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    todo!()
+use anyhow::{Result, bail};
+use pico_args::Arguments;
+use std::path::PathBuf;
+
+fn main() -> Result<()> {
+    let mut args = Arguments::from_env();
+
+    if args.contains(["-h", "--help"]) {
+        println!(
+            "nokozero
+-h, --help        print this message
+-d, --game-dir    path to directory containing game files"
+        );
+        return Ok(());
+    }
+
+    let game_dir_path: PathBuf = args.value_from_str(["-d", "--game-dir"])?;
+
+    if !game_dir_path.is_dir() {
+        bail!("`-d`/`--game-dir`: path does not point to a directory");
+    }
+
+    Ok(())
 }
